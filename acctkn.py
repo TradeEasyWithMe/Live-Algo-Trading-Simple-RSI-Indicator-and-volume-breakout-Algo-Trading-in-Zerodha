a72d633acb4408332ddef816190dee6ca6c96e53
TradeEasyWithMe <79126019+TradeEasyWithMe@users.noreply.github.com>
2021-03-01 16:49:01 +0530

    Add files via upload

---
+++ acctkn.py
@@ -0,0 +1,9 @@
+#!c:\users\pcuser1.pcuser1-pc\appdata\local\programs\python\python38\python.exe
+
+def att():
+    at = 'put yout access token here'
+    return at
+
+def atp():
+    ap = 'api key here'
+    return ap
